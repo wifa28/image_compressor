a6398458e0dd73fb2a22c2de39350a753bd2ec0b
Wafiq <wafiq.afifah@inadigital.co.id>
2026-01-12 12:55:20 +0700

    fix: adjustment header function

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,42 +1,38 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Header() {
+
+  const pathname = usePathname();
+
+  const linkClass = (path: string) =>
+    pathname === path
+      ? "px-3 py-1 rounded-full bg-[#6E026F] text-white"
+      : "px-3 py-1 rounded-full text-[#6E026F] hover:bg-[#6E026F]/10 transition";
+
   return (
     <header className="bg-white shadow-sm h-16">
-      <div className="max-w-6xl mx-auto h-full px-4 flex items-center justify-between">
+      <div className="max-w-6xl mx-auto px-4 h-full flex items-center justify-between">
 
-        {/* LOGO + BRAND */}
+        {/* LOGO */}
         <Link href="/" className="flex items-center gap-3">
-          {/* <img
-            src="/logo.png"
-            alt="ImageTools Logo"
-            className="h-20 w-20 object-contain"
-          /> */}
+          {/* <img src="/logo.png" alt="Logo" className="h-8 w-auto" /> */}
           <span className="text-xl font-[1000] text-[#6E026F]">
             ImageTools
           </span>
         </Link>
 
         {/* NAV */}
-        <nav className="flex items-center gap-2 text-sm font-medium">
-          <Link
-            href="/"
-            className="px-4 py-1.5 rounded-full bg-[#6E026F] text-white"
-          >
+        <nav className="flex gap-2">
+          <Link href="/" className={linkClass("/")}>
             Home
           </Link>
-
-          <Link
-            href="/compress"
-            className="px-4 py-1.5 rounded-full text-[#6E026F] hover:bg-[#6E026F]/10 transition"
-          >
+          <Link href="/compress" className={linkClass("/compress")}>
             Compress
           </Link>
-
-          {/* <Link
-            href="/convert"
-            className="px-4 py-1.5 rounded-full text-[#6E026F] hover:bg-[#6E026F]/10 transition"
-          >
+          {/* <Link href="/convert" className={linkClass("/convert")}>
             Convert
           </Link> */}
         </nav>
